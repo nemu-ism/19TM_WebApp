bfa3e2d08bf74109e8f124669b107da59f98580f
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2019-03-13 21:54:15 +0900

    FileAPIでtxtファイル読み込み

--- controller/FileController.js
+++ controller/FileController.js
@@ -1,11 +1,16 @@
-﻿function TextInput() {
-    var fso = new ActiveXObject("Scripting.FileSystemObject");
-    var file = fso.OpenTextFile("lylics.txt");
+﻿var file = document.querySelector('#getfile');
 
-    while(!file.AtEndOfStream) {
-        text += file.ReadLine();
-    }
-}
+file.onchange = function (){
+  var fileList = file.files;
+  //読み込み
+  var reader = new FileReader();
+  reader.readAsText(fileList[0]);
+
+  //読み込み後
+  reader.onload = function  () {
+    document.querySelector('#display').textContent = reader.result;
+  };
+};
 
 /*
 var fs = require('fs');
