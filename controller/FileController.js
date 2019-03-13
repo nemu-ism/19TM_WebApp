42efd1115135e710083b9d8cef13a3d4279dbdc1
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2019-03-13 22:04:44 +0900

    整形

--- controller/FileController.js
+++ controller/FileController.js
@@ -1,30 +1,4 @@
-﻿var file = document.querySelector('#getfile');
-
-file.onchange = function (){
-  var fileList = file.files;
-  //読み込み
-  var reader = new FileReader();
-  reader.readAsText(fileList[0]);
-
-  //読み込み後
-  reader.onload = function  () {
-    document.querySelector('#display').textContent = reader.result;
-  };
-};
-
-/*
-var fs = require('fs');
-var readline = require("readline");
-
-var text = fs.readFileSync("lylics.txt");
-var lines = text.toString().split('¥n');
-
-for (var line of lines) {
-  console.log(line)
-}
-*/
-
-function FileWriter() {
+﻿function FileWriter() {
     var Stream = document.getElementById("textarea_id").value;
     var blob = new Blob([Stream], { "type": "text/plain" });
     if (window.navigator.msSaveOrOpenBlob) {
--- controller/MovieController.js
+++ controller/MovieController.js
@@ -1,24 +1,24 @@
 // IFrame Player API コード を読み込む
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/iframe_api";
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-  // IFrame Player API コードが読み込まれると onYouTubeIframeAPIReady関数が実行される
-  var player;
-  function onYouTubeIframeAPIReady() {
-   // 動画プレーヤーを挿入する YT.Player オブジェクトを作成する
-   player = new YT.Player( 'player', {
-    width: '960',
-    height: '540',
-    videoId: 'dBdEb4XVLqs', // YouTube動画ID
-    events: {
-     'onReady':onPlayerReady // 準備ができたら再生を始めるように指示
-    }
-   });
-  }
+// IFrame Player API コードが読み込まれると onYouTubeIframeAPIReady関数が実行される
+var player;
+function onYouTubeIframeAPIReady() {
+    // 動画プレーヤーを挿入する YT.Player オブジェクトを作成する
+    player = new YT.Player( 'player', {
+        width: '960',
+        height: '540',
+        videoId: 'dBdEb4XVLqs', // YouTube動画ID
+        events: {
+            'onReady':onPlayerReady // 準備ができたら再生を始めるように指示
+        }
+    });
+}
 
-  // 再生を開始する関数
-  function onPlayerReady(event) {
-   event.target.playVideo();
-  }
+// 再生を開始する関数
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
