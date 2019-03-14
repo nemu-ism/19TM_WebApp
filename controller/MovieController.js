087e1f3387d8c2438f8c945f923bda5df01662eb
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2019-03-15 06:00:53 +0900

    更新でとりあえず動画切り替え

--- controller/MovieController.js
+++ controller/MovieController.js
@@ -4,6 +4,8 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+videoId = 'dBdEb4XVLqs'
+
 // IFrame Player API コードが読み込まれると onYouTubeIframeAPIReady関数が実行される
 var player;
 function onYouTubeIframeAPIReady() {
@@ -11,11 +13,27 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player( 'player', {
         width: '760', //960
         height: '540', //540
-        videoId: 'dBdEb4XVLqs', // YouTube動画ID
+        videoId: videoId, // YouTube動画ID
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
     });
 }
 
-// 再生を開始する関数
-function onPlayerReady(event) {
-    event.target.playVideo();
+function ply() {
+    player.destroy();
+    player = new YT.Player( 'player', {
+        width: '760', //960
+        height: '540', //540
+        videoId: 'EKUBLenVvH4', // YouTube動画ID
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerStateChange(event) {
+    if(event.data == YT.PlayerState.PLAYING && !done){
+        event.target.loadVideoById(videoId);
+    }
 }
