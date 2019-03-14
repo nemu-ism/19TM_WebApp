65452aea0c06b3de0638d3b622ed877dece5297a
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2019-03-15 05:07:27 +0900

    id名修正

--- controller/FileController.js
+++ controller/FileController.js
@@ -1,5 +1,5 @@
 ﻿function FileWriter() {
-    var Stream = document.getElementById("textarea_id").value;
+    var Stream = document.getElementById("input").value;
     var blob = new Blob([Stream], { "type": "text/plain" });
     if (window.navigator.msSaveOrOpenBlob) {
         window.navigator.msSaveOrOpenBlob(blob, "lylics.txt");
