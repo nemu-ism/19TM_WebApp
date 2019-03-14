7433f220ea36f92005c5aaa78a7ffc8913258e2e
nemu-ism <vanilla_chip_s@yahoo.co.jp>
2019-03-15 06:09:02 +0900

    loadVideo()で動画操作

--- controller/MovieController.js
+++ controller/MovieController.js
@@ -4,36 +4,19 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-videoId = 'dBdEb4XVLqs'
-
 // IFrame Player API コードが読み込まれると onYouTubeIframeAPIReady関数が実行される
 var player;
 function onYouTubeIframeAPIReady() {
-    // 動画プレーヤーを挿入する YT.Player オブジェクトを作成する
+    loadVideo('dBdEb4XVLqs');
+}
+
+function loadVideo(videoId) {
+    if(player) {
+        player.destroy();
+    }
     player = new YT.Player( 'player', {
         width: '760', //960
         height: '540', //540
         videoId: videoId, // YouTube動画ID
-        events: {
-            'onStateChange': onPlayerStateChange
-        }
     });
-}
-
-function ply() {
-    player.destroy();
-    player = new YT.Player( 'player', {
-        width: '760', //960
-        height: '540', //540
-        videoId: 'EKUBLenVvH4', // YouTube動画ID
-        events: {
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
-
-function onPlayerStateChange(event) {
-    if(event.data == YT.PlayerState.PLAYING && !done){
-        event.target.loadVideoById(videoId);
-    }
 }
